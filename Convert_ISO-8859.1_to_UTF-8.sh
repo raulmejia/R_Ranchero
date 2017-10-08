29e16d62a982017509764eeb707cf83d9b90cdcb
raulmejia <raulmejia@users.noreply.github.com>
2017-10-08 12:21:46 -0500

    convertir un archivo ISO... a utf8

--- Convert_ISO-8859.1_to_UTF-8.sh
+++ Convert_ISO-8859.1_to_UTF-8.sh
@@ -1,1 +1,5 @@
 
+# Para ver el tipo de archivos que tenemos
+file -i *
+iconv -f iso-8859-1 -t utf-8 patharchivoviejo > pathmyarchivonuevo
+# Ejemplo iconv -f iso-8859-1 -t utf-8 Faltante__H00_hasta_H95_INEGI_Exporta_20171007172107.csv.bk > Faltante__H00_hasta_H95_INEGI_Exporta_20171007172107.csv.bk.utf8
